70bd60d4781b72616af4e096e68a1ebaf54feac8
Theo <kkw.theo@gmail.com>
2025-04-17 17:00:08 +0900

    Init:init

---
+++ src/main/java/kr/co/lunatalk/LunatalkServerApplication.java
@@ -0,0 +1,13 @@
+package kr.co.lunatalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunatalkServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LunatalkServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/lunatalk/LunatalkServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.lunatalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunatalkServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

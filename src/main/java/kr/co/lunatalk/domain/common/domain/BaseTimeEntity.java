d1c8aa54d91173f6cf06848ad71a96d707701d33
theo-ooooo <kkw.theo@gmail.com>
2025-04-22 21:26:19 +0900

    feature: BaseTimeEntity 추가

---
+++ src/main/java/kr/co/lunatalk/domain/common/domain/BaseTimeEntity.java
@@ -0,0 +1,24 @@
+package kr.co.lunatalk.domain.common.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public class BaseTimeEntity {
+
+	@CreatedDate
+	@Column(nullable = false)
+	private LocalDateTime createdAt;
+
+	@LastModifiedBy
+	private LocalDateTime updatedAt;
+}

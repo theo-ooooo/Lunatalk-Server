005a62f1091b5025cfd63b9feb6267738a1dc848
theo-ooooo <kkw.theo@gmail.com>
2025-04-24 18:04:02 +0900

    chore: querydsl 설정

--- src/main/java/kr/co/lunatalk/domain/member/repository/MemberRepository.java
+++ src/main/java/kr/co/lunatalk/domain/member/repository/MemberRepository.java
@@ -3,5 +3,5 @@ package kr.co.lunatalk.domain.member.repository;
 import kr.co.lunatalk.domain.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
 }

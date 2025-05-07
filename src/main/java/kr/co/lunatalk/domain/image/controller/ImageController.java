fd5f377d8bccf4f50485e754ad37a905711a1062
theo-ooooo <kkw.theo@gmail.com>
2025-05-07 23:30:36 +0900

    chore: 이미지 스웨거 정보 추가

--- src/main/java/kr/co/lunatalk/domain/image/controller/ImageController.java
+++ src/main/java/kr/co/lunatalk/domain/image/controller/ImageController.java
@@ -1,5 +1,7 @@
 package kr.co.lunatalk.domain.image.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import kr.co.lunatalk.domain.image.dto.request.ProductImageCompletedRequest;
 import kr.co.lunatalk.domain.image.dto.request.ProductImageUploadRequest;
@@ -12,20 +14,24 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @PreAuthorize("hasRole('ROLE_ADMIN')")
+@Tag(name = "이미지 업로드/삭제", description = "이미지 관련 API")
 public class ImageController {
 	private final ImageService imageService;
 
 	@PostMapping("/products/upload-url")
+	@Operation(summary = "이미지 Presigned url 생성",description = "presigned url를 생성합니다.")
 	public PresignedUrlResponse productPresignedUrlCreate(@RequestBody @Valid ProductImageUploadRequest request) {
 		return imageService.productImageUpload(request);
 	}
 
 	@PostMapping("/products/upload-complete")
+	@Operation(summary = "생성한 presigned url 완료", description = "생성한 presigned url 완료 처리합니다.")
 	public void productImageUploaded(@RequestBody @Valid ProductImageCompletedRequest request) {
 		imageService.productImageCompleteUpload(request);
 	}
 
 	@DeleteMapping("/images/delete/{imageKey}")
+	@Operation(summary = "이미지 삭제", description = "업로드된 이미지를 삭제합니다.")
 	public void deleteImage(@PathVariable String imageKey) {
 		imageService.deleteByImageKey(imageKey);
 	}

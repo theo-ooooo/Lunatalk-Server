577a915a1a10d65fb34ba7c7b73b376cc15fb3b5
theo-ooooo <kkw.theo@gmail.com>
2025-05-09 15:52:35 +0900

    refactor: cors 정책 수정

--- src/main/java/kr/co/lunatalk/global/common/constants/UrlConstants.java
+++ src/main/java/kr/co/lunatalk/global/common/constants/UrlConstants.java
@@ -9,6 +9,13 @@ public enum UrlConstants {
 	PROD_SERVER_URL("https://api.lunatalk.co.kr"),
 	DEV_SERVER_URL("https://dev-api.lunatalk.co.kr"),
 	LOCAL_SERVER_URL("http://localhost:8080"),
+
+
+	PROD_DOMAIN_URL("https://www.lunatalk.co.kr"),
+	PROD_DOMAIN_ADMIN_URL("https://admin.lunatalk.co.kr"),
+	DEV_DOMAIN_URL("https://dev.lunatalk.co.kr"),
+	DEV_DOMAIN_ADMIN_URL("https://dev-admin.lunatalk.co.kr"),
+	LOCAL_DOMAIN_URL("http://localhost:5173")
 	;
 	private String value;
 }

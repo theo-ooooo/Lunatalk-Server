2656ade86da349e8d50e648f381bedcdcc6a7c05
theo-ooooo <kkw.theo@gmail.com>
2025-05-12 16:47:54 +0900

    refactor: 주문 전체 조회 관리자 롤 적용

--- src/main/java/kr/co/lunatalk/domain/order/controller/OrderController.java
+++ src/main/java/kr/co/lunatalk/domain/order/controller/OrderController.java
@@ -16,6 +16,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -51,6 +52,7 @@ public class OrderController {
 	//관리자용
 	@GetMapping()
 	@Operation(description = "전체 주문 조회")
+	@PreAuthorize("hasRole('ADMIN')")
 	public Page<OrderListResponse> getOrders(
 		@RequestParam(required = false) String orderNumber,
 		@RequestParam(required = false) OrderStatus status,

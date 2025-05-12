4553c981bdb75659780c9ff543ff22bf4498736d
theo-ooooo <kkw.theo@gmail.com>
2025-05-12 11:04:42 +0900

    refactor: 주문 dto 주문일 추가

--- src/main/java/kr/co/lunatalk/domain/order/dto/response/OrderFIndResponse.java
+++ src/main/java/kr/co/lunatalk/domain/order/dto/response/OrderFIndResponse.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import kr.co.lunatalk.domain.order.domain.Order;
 import kr.co.lunatalk.domain.order.domain.OrderStatus;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public record OrderFIndResponse(
@@ -11,7 +12,8 @@ public record OrderFIndResponse(
 	@Schema(description = "주문 번호", defaultValue = "L3ABCDEFG") String orderNumber,
 	@Schema(description = "주문 상태", defaultValue = "ORDERED") OrderStatus status,
 	@Schema(description = "주문 총 금액", defaultValue = "1000") Long totalPrice,
-	@Schema(description = "구매 상품들")List<OrderItemDto> orderItems
+	@Schema(description = "구매 상품들") List<OrderItemDto> orderItems,
+	@Schema(description = "주문일")LocalDateTime createdAt
 	) {
 
 	public static OrderFIndResponse from(Order order) {
@@ -20,7 +22,8 @@ public record OrderFIndResponse(
 			order.getOrderNumber(),
 			order.getStatus(),
 			order.getTotalPrice(),
-			order.getOrderItems().stream().map(OrderItemDto::from).toList()
+			order.getOrderItems().stream().map(OrderItemDto::from).toList(),
+			order.getCreatedAt()
 		);
 	}
 }

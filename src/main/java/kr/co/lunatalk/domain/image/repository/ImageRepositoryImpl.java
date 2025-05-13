289286a5734f0db7807e7036dbec40fa7afc7865
theo-ooooo <kkw.theo@gmail.com>
2025-05-13 22:31:39 +0900

    refactor: image select imageStatus completed만 조회하도록!

--- src/main/java/kr/co/lunatalk/domain/image/repository/ImageRepositoryImpl.java
+++ src/main/java/kr/co/lunatalk/domain/image/repository/ImageRepositoryImpl.java
@@ -1,7 +1,9 @@
 package kr.co.lunatalk.domain.image.repository;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import kr.co.lunatalk.domain.image.domain.Image;
+import kr.co.lunatalk.domain.image.domain.ImageStatus;
 import kr.co.lunatalk.domain.image.domain.ImageType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -19,7 +21,8 @@ public class ImageRepositoryImpl implements ImageRepositoryCustom {
 		return queryFactory
 			.selectFrom(image)
 			.where(image.referenceId.eq(productId)
-				.and(image.imageType.in(ImageType.PRODUCT_CONTENT, ImageType.PRODUCT_THUMBNAIL)))
+				.and(image.imageType.in(ImageType.PRODUCT_CONTENT, ImageType.PRODUCT_THUMBNAIL)),
+				isImageCompleted())
 			.fetch();
 	}
 
@@ -27,7 +30,12 @@ public class ImageRepositoryImpl implements ImageRepositoryCustom {
 		return queryFactory
 			.selectFrom(image)
 			.where(image.referenceId.in(productIds)
-				.and(image.imageType.in(ImageType.PRODUCT_CONTENT, ImageType.PRODUCT_THUMBNAIL)))
+				.and(image.imageType.in(ImageType.PRODUCT_CONTENT, ImageType.PRODUCT_THUMBNAIL)),
+				isImageCompleted())
 			.fetch();
+	}
+
+	private BooleanExpression isImageCompleted() {
+		return image.imageStatus.eq(ImageStatus.COMPLETED);
 	}
 }

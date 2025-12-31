d0f9dabf2f0f9a4590557a1b4aa829219b872669
theo-ooooo <kkw.theo@gmail.com>
2025-12-31 22:11:07 +0900

    feat: 문의 관련 ErrorCode 추가
    - INQUIRY_NOT_FOUND
    - INQUIRY_REPLY_NOT_FOUND
    - INQUIRY_ALREADY_ANSWERED
    - INQUIRY_UNAUTHORIZED

--- src/main/java/kr/co/lunatalk/global/exception/ErrorCode.java
+++ src/main/java/kr/co/lunatalk/global/exception/ErrorCode.java
@@ -59,6 +59,12 @@ public enum ErrorCode {
 	// CartItem
 	CART_ITEM_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 장바구니를 찾을수 없습니다."),
 
+	// Inquiry
+	INQUIRY_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 문의를 찾을 수 없습니다."),
+	INQUIRY_REPLY_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 답변을 찾을 수 없습니다."),
+	INQUIRY_ALREADY_ANSWERED(HttpStatus.CONFLICT, "이미 답변이 완료된 문의입니다."),
+	INQUIRY_UNAUTHORIZED(HttpStatus.FORBIDDEN, "본인의 문의만 조회/수정할 수 있습니다."),
+
 	;
 	private HttpStatus httpStatus;
 	private String message;
